a113b80ac81b4d789a276eb65fee4cff1bf46bd5
Diego Zanetti <di.zanetti@outlook.com>
2022-05-10 18:30:16 +0100

    fuzz

--- fuzzing/main_test.go
+++ fuzzing/main_test.go
@@ -9,7 +9,7 @@ func TestAdd(t *testing.T) {
 	got := Add(4, 6)
 	want := 10
 
-	if got != want {
+	if Compare(got, want) {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
 }
@@ -17,6 +17,15 @@ func TestAdd(t *testing.T) {
 func FuzzAdd(f *testing.F) {
 	f.Fuzz(
 		func(t *testing.T, a int, b int) {
-			Add(a, b)
+			got := Add(a, b)
+			want := a + b
+
+			if Compare(got, want) {
+				t.Errorf("got %q, wanted %q", got, want)
+			}
 		})
+}
+
+func Compare(got, want int) bool {
+	return got != want
 }
