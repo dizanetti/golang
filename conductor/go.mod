745739829e4c66833512092ba89f58badfb99a41
Diego Zanetti <di.zanetti@outlook.com>
2022-07-25 15:36:15 +0100

    conductor

--- conductor/go.mod
+++ conductor/go.mod
@@ -2,4 +2,18 @@ module conductor-lab
 
 go 1.18
 
-require github.com/conductor-sdk/conductor-go v1.2.2 // indirect
+require (
+	github.com/antihax/optional v1.0.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/conductor-sdk/conductor-go v1.2.3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- conductor/main.go
+++ conductor/main.go
@@ -2,39 +2,105 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
 
 	"github.com/conductor-sdk/conductor-go/sdk/client"
 	"github.com/conductor-sdk/conductor-go/sdk/model"
 	"github.com/conductor-sdk/conductor-go/sdk/settings"
-	"github.com/conductor-sdk/conductor-go/sdk/worker"
+	"github.com/conductor-sdk/conductor-go/sdk/workflow"
 	"github.com/conductor-sdk/conductor-go/sdk/workflow/executor"
+	"github.com/sirupsen/logrus"
 )
 
 var (
 	apiClient = client.NewAPIClient(
 		settings.NewAuthenticationSettings(
-			os.Getenv("KEY"),
-			os.Getenv("SECRET"),
+			"",
+			"",
 		),
 		settings.NewHttpSettings(
-			os.Getenv("CONDUCTOR_SERVER_URL"),
+			"http://localhost:8080/api",
 		))
-	taskRunner       = worker.NewTaskRunnerWithApiClient(apiClient)
+	//taskRunner       = worker.NewTaskRunnerWithApiClient(apiClient)
 	workflowExecutor = executor.NewWorkflowExecutor(apiClient)
 	metadataClient   = client.MetadataResourceApiService{APIClient: apiClient}
 )
 
 func main() {
+	//taskRunner.StartWorker("task1", Task1, 1, time.Millisecond*100)
+	//taskRunner.StartWorker("task2", Task2, 1, time.Millisecond*100)
 
+	logrus.Info("Started Workers")
+
+	//taskRunner.WaitWorkers()
+
+	conductorWorkflow := workflow.NewConductorWorkflow(workflowExecutor).
+		Name("my_first_workflow").
+		Version(1).
+		OwnerEmail("developers@orkes.io")
+
+	conductorWorkflow.Add(workflow.NewSimpleTask("simple_task_2", "simple_task_1")).Add(workflow.NewSimpleTask("simple_task_1", "simple_task_2"))
+
+	//Register the workflow with server
+	conductorWorkflow.Register(true)
+
+	//Input can be either a map or a struct that is serializable to a JSON map
+	workflowInput := map[string]interface{}{}
+
+	workflowId, err := workflowExecutor.StartWorkflow(&model.StartWorkflowRequest{
+		Name:  conductorWorkflow.GetName(),
+		Input: workflowInput,
+	})
+
+	logrus.Info(workflowId)
+	logrus.Info(err)
 }
 
-func StartWorkers() {
-	taskRunner.StartWorker("task1", Task1, 1, time.Millisecond*100)
-	taskRunner.StartWorker("task2", Task2, 1, time.Millisecond*100)
+//NameAndCity struct that represents the input to the workflow
+type NameAndCity struct {
+	Name string
+	City string
 }
 
+func GetTaskDefinitions() []model.TaskDef {
+	taskDefs := []model.TaskDef{
+		{Name: "task1", TimeoutSeconds: 60},
+		{Name: "task2", TimeoutSeconds: 60},
+	}
+	return taskDefs
+}
+
+//NewSimpleWorkflow Create a simple 2-step workflow and register it with the server
+/*func NewSimpleWorkflow(executor *executor.WorkflowExecutor) *ConductorWorkflow {
+
+	wf := NewConductorWorkflow(executor).
+		Name("simple_workflow").
+		Version(1).
+		Description("Simple Two Step Workflow").
+		TimeoutPolicy(TimeOutWorkflow, 600)
+
+	//Task1
+	task1 := NewSimpleTask("task1", "task1").
+		Input("name", "${workflow.input.Name}")
+
+	//Task 2
+	task2 := NewSimpleTask("task2", "task2").
+		Input("city", "${workflow.input.City}")
+
+	//Add two simple tasks
+	wf.
+		Add(task1).
+		Add(task2)
+
+	//Add the output of the workflow from the two tasks
+	wf.OutputParameters(map[string]interface{}{
+		"Greetings": task1.OutputRef("greetings"),
+		"ZipCode":   task2.OutputRef("zip"),
+	})
+
+	return wf
+}*/
+
+//Task1 worker for Task1
 func Task1(task *model.Task) (interface{}, error) {
 
 	//To fail the task send an error
@@ -45,6 +111,7 @@ func Task1(task *model.Task) (interface{}, error) {
 	}, nil
 }
 
+//Task2 worker for Task2
 func Task2(task *model.Task) (interface{}, error) {
 	return map[string]interface{}{
 		"zip": "10121",
